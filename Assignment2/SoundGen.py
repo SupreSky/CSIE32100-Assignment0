449db4b85298e601b176f130c3e90d382b3e46bb
SupreSky <nanan2468@yahoo.com.tw>
2017-12-11 17:03:20 +0800

    Mapping table level 2 and 4

--- Assignment2/SoundGen.py
+++ Assignment2/SoundGen.py
@@ -6,3 +6,43 @@ def sinPhase(f, framerate, time):
     t = np.linspace(0, time, num=nframes)
     phase = 2 * np.pi * f * t
     return np.sin(phase)
+
+def note2(num):
+    if int(num) == 1:
+        freq = 65
+    elif int(num) == 2:
+        freq = 73
+    elif int(num) == 3:
+        freq = 82
+    elif int(num) == 4:
+        freq = 87
+    elif int(num) == 5:
+        freq = 98
+    elif int(num) == 6:
+        freq = 110
+    elif int(num) == 7:
+        freq = 124
+    else:
+        freq = 10**9
+        print ("ERROR!!")
+    return freq
+
+def note4(num):
+    if int(num) == 1:
+        freq = 262
+    elif int(num) == 2:
+        freq = 294
+    elif int(num) == 3:
+        freq = 330
+    elif int(num) == 4:
+        freq = 349
+    elif int(num) == 5:
+        freq = 393
+    elif int(num) == 6:
+        freq = 440
+    elif int(num) == 7:
+        freq = 494
+    else:
+        freq = 10**9
+        print ("ERROR!!")
+    return freq
