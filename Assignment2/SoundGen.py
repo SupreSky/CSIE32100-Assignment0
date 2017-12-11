46808aae6a29d23bb2dd5dc9f2fa2d460910f254
SupreSky <nanan2468@yahoo.com.tw>
2017-12-11 16:56:46 +0800

    def sinPhase

--- Assignment2/SoundGen.py
+++ Assignment2/SoundGen.py
@@ -1,2 +1,8 @@
 import wave
 import numpy as np
+
+def sinPhase(f, framerate, time):
+    nframes = time * framerate
+    t = np.linspace(0, time, num=nframes)
+    phase = 2 * np.pi * f * t
+    return np.sin(phase)
