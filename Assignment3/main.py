a0c37803138852e564a0711349d59f27a6a5efef
SupreSky <nanan2468@yahoo.com.tw>
2018-01-29 00:12:28 +0800

    First Commit

---
+++ Assignment3/main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+
+
+P = 31
+img = cv2.imread("../Assignment3/images/i1.pgm", 0)
+
+
+def MAD(x, y, i, j):
+    dSum = np.zeros
+    for k in range(16):
+        for l in range(16):
+            C = img[x:x+k, y:y+l]
+            R = img[x+i:x+i+k, y+j:y+j+l]
+            dSum = dSum + np.absolute(C-R)
+    ans = dSum / 256
+    return ans
+    
+            
+
+def seq_search(x, y):
+    minMAD = 1e9
+    for i in range(-15, 15+1):
+        for j in range(-15, 15+1):
+            curMAD = MAD(x, y, i, j)
+            if curMAD < minMAD:
+                minMAD = curMAD
+                u = i
+                v = j
+    return (u,v)
+
+for x in range(img.shape[0]):
+    for y in range(img.shape[1]):
+        print (seq_search(x, y) )
+                
+        
+
+#img2 = cv2.imread("../Assignment3/images/i1.pgm")
+
+
+#print(img.shape)
+#print(img2.shape)
+#print(img3.shape)
+
+
+#cv2.imshow('image',img-img2)
+#cv2.imshow('image1',img)
+#cv2.imshow('image2',img2)
+#cv2.imshow('image3',img3)
+#cv2.imshow('image3',cv2.absdiff(img, img2))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
