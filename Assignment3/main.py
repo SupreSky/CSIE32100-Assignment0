bee3ad7cd41dd6feac890e6b7b56d0b7eed90f77
SupreSky <nanan2468@yahoo.com.tw>
2018-01-30 01:47:55 +0800

    2D Search

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -11,7 +11,7 @@ img2 = cv2.imread("../Assignment3/images/i2.pgm", 0)
 img3 = img
 
 
-def MAD(x, y, i, j):
+def MAD_1(x, y, i, j):
     dSum = 0
 #    cK = -8
 #    cL = -8
@@ -23,6 +23,15 @@ def MAD(x, y, i, j):
             R = img[k, l]
             dSum = dSum + abs(C-R)
     return dSum
+
+def MAD_2(R, C):
+    cSum = 0
+    minSum = 1e9
+    for i in range(0, 9):
+        cSum = cSum + abs(C[i]-R[i])
+        if cSum < minSum:
+            minSum = cSum
+    return minSum
     
             
 
@@ -38,7 +47,7 @@ def seq_search():
                     
                     if j < 0 or j+7 >= img.shape[1]:
                         continue
-                    curMAD = MAD(x, y, i, j)
+                    curMAD = MAD_1(x, y, i, j)
                     if curMAD < minMAD:
                         minMAD = curMAD
                         u = i
@@ -49,11 +58,47 @@ def seq_search():
             for a in range(u-8, u+8):
                 for b in range(v-8, v+8):
                     img3[x+a-u, y+b-v] = img[a,b]
+
+def twoD_search():
+    for x in range(7, img.shape[0], 16):
+        for y in range(7, img.shape[1], 16):
+            flag = False
+            oSet = 8
+            nine = np.zeros(9)
+            nine2 = np.zeros(9)
+            print(x+oSet, y+oSet)
+            nine[0] = img[x-oSet, y-oSet]
+            nine[1] = img[x, y-oSet]
+            nine[2] = img[x+oSet, y-oSet]
+            nine[3] = img[x-oSet, y]
+            nine[4] = img[x, y]
+            nine[5] = img[x+oSet, y]
+            nine[6] = img[x-oSet, y+oSet]
+            nine[7] = img[x, y+oSet]
+            nine[8] = img[x+oSet, y+oSet]
             
-#            print(x*100/img.shape[0], y*100/img.shape[1])
+            nine2[0] = img2[x-oSet, y-oSet]
+            nine2[1] = img2[x, y-oSet]
+            nine2[2] = img2[x+oSet, y-oSet]
+            nine2[3] = img2[x-oSet, y]
+            nine2[4] = img2[x, y]
+            nine2[5] = img2[x+oSet, y]
+            nine2[6] = img2[x-oSet, y+oSet]
+            nine2[7] = img2[x, y+oSet]
+            nine2[8] = img2[x+oSet, y+oSet]
+            
+            while flag == False:
+                minSum = MAD_2(nine, nine2)
+                if oSet == 1:
+                    flag = True
+                oSet = oSet / 2
+                
+                
+                
                     
 timeStart = time.time()
-seq_search()
+#seq_search()
+twoD_search()
 timeEnd = time.time()
 print("Time: ")
 print(timeEnd-timeStart)
