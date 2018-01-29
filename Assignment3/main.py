8e9e7dfcf88a93044859b52708934494fc1abd49
SupreSky <nanan2468@yahoo.com.tw>
2018-01-29 19:38:45 +0800

    some edit1

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -4,6 +4,7 @@ import cv2
 
 P = 31
 img = cv2.imread("../Assignment3/images/i1.pgm", 0)
+img2 = cv2.imread("../Assignment3/images/i2.pgm", 0)
 
 
 def MAD(x, y, i, j):
@@ -20,8 +21,12 @@ def MAD(x, y, i, j):
 
 def seq_search(x, y):
     minMAD = 1e9
-    for i in range(-15, 15+1):
-        for j in range(-15, 15+1):
+    for i in range(x-15, x+16):
+        if i < 0 or i >= img.shape[0]:
+            continue
+        for j in range(y-15, y+16):
+            if j < 0 or j >= img.shape[1]:
+                continue
             curMAD = MAD(x, y, i, j)
             if curMAD < minMAD:
                 minMAD = curMAD
@@ -31,7 +36,7 @@ def seq_search(x, y):
 
 for x in range(img.shape[0]):
     for y in range(img.shape[1]):
-        print (seq_search(x, y) )
+        print ( seq_search(x, y) )
                 
         
 
