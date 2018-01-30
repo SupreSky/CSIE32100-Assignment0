d4c2a2b33dee5b02b0a9540be82229dc83b1198d
SupreSky <nanan2468@yahoo.com.tw>
2018-01-30 10:31:12 +0800

    bugs fix

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -8,30 +8,33 @@ P = 31
 
 img = cv2.imread("../Assignment3/images/i1.pgm", 0)
 img2 = cv2.imread("../Assignment3/images/i2.pgm", 0)
-img3 = img
+img3 = np.copy(img)
+#img4 = np.copy(img)
+img4 = np.zeros((img.shape[0], img.shape[1]))
 
 
 def MAD_1(x, y, i, j):
     dSum = 0
-#    cK = -8
-#    cL = -8
     for k in range(i-8, i+8):
         for l in range(j-8, j+8):
-#            if x == 8 and y == 312 and i == 0 and j == 313:
-#                print(x, y, i, j, k, l)
             C = img2[x+k-i,y+l-j]
             R = img[k, l]
             dSum = dSum + abs(C-R)
     return dSum
 
 def MAD_2(R, C):
-    cSum = 0
-    minSum = 1e9
-    for i in range(0, 9):
-        cSum = cSum + abs(C[i]-R[i])
-        if cSum < minSum:
-            minSum = cSum
-    return minSum
+    cAns = 0
+    minAns = 1e9
+    for i in range(0, 3):
+        for j in range(0, 3):
+            cAns = abs(C[i][j]-R[i][j])
+            if cAns < minAns:
+                minAns = cAns
+                u = i
+                v = j
+    u = u - 1
+    v = v - 1
+    return u, v
     
             
 
@@ -58,59 +61,58 @@ def seq_search():
             for a in range(u-8, u+8):
                 for b in range(v-8, v+8):
                     img3[x+a-u, y+b-v] = img[a,b]
+                    
 
 def twoD_search():
     for x in range(7, img.shape[0], 16):
         for y in range(7, img.shape[1], 16):
             flag = False
             oSet = 8
-            nine = np.zeros(9)
-            nine2 = np.zeros(9)
-            print(x+oSet, y+oSet)
-            nine[0] = img[x-oSet, y-oSet]
-            nine[1] = img[x, y-oSet]
-            nine[2] = img[x+oSet, y-oSet]
-            nine[3] = img[x-oSet, y]
-            nine[4] = img[x, y]
-            nine[5] = img[x+oSet, y]
-            nine[6] = img[x-oSet, y+oSet]
-            nine[7] = img[x, y+oSet]
-            nine[8] = img[x+oSet, y+oSet]
-            
-            nine2[0] = img2[x-oSet, y-oSet]
-            nine2[1] = img2[x, y-oSet]
-            nine2[2] = img2[x+oSet, y-oSet]
-            nine2[3] = img2[x-oSet, y]
-            nine2[4] = img2[x, y]
-            nine2[5] = img2[x+oSet, y]
-            nine2[6] = img2[x-oSet, y+oSet]
-            nine2[7] = img2[x, y+oSet]
-            nine2[8] = img2[x+oSet, y+oSet]
-            
+            nine = np.zeros((3,3))
+            nine2 = np.zeros((3,3))
+            k = x
+            l = y
             while flag == False:
-                minSum = MAD_2(nine, nine2)
+                nine = img[[k-oSet, k, k+oSet], :][:, [l-oSet, l, l+oSet]]
+                nine2 = img2[[k-oSet, k, k+oSet], :][:, [l-oSet, l, l+oSet]]
+                
+                u, v = MAD_2(nine, nine2)
                 if oSet == 1:
                     flag = True
-                oSet = oSet / 2
+                oSet = int(oSet / 2)
+                k = k + (u * oSet)
+                l = l + (v * oSet)
+            
+            for a in range(k-8, k+8):
+                for b in range(l-8, l+8):
+#                    print(a,b)
+                    if a >= img.shape[0] or b >= img.shape[1]:
+                        continue
+                    img4[a-k, b-l] = img[a, b]
                 
                 
                 
                     
 timeStart = time.time()
 #seq_search()
+timeEnd = time.time()
+print("Seq Time: ")
+print(timeEnd-timeStart)
+           
+timeStart = time.time()
 twoD_search()
 timeEnd = time.time()
-print("Time: ")
+print("2D Time: ")
 print(timeEnd-timeStart)
-                    
-print(img3)
 
-cv2.imwrite("/images/i2p.pgm", img3)
+cv2.imwrite("../Assignment3/images/i2p_Seq.pgm", img3)
+cv2.imwrite("../Assignment3/images/i2p_2D.pgm", img4)
                 
 
 #cv2.imshow('image1',img)
 #cv2.imshow('image2',img2)
 cv2.imshow('image3',img3)
+cv2.imshow('image4',img4)
 #cv2.imshow('image3',cv2.absdiff(img, img2))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
